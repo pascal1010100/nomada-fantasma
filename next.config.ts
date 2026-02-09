f0a0cf09ce8d2cb2bc09dcd244c1099455e7f8db
Jose Aguilar <123897068+pascal1010100@users.noreply.github.com>
2026-02-09 16:05:01 -0600

    Fix React Email components build error by adding transpilePackages config

--- next.config.ts
+++ next.config.ts
@@ -4,10 +4,9 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
-
-
   // Reemplaza "experimental.turbo" por "turbopack"
   turbopack: {}, // sin "disabled"
+  transpilePackages: ['@react-email/components'],
   images: {
     remotePatterns: [
       {
